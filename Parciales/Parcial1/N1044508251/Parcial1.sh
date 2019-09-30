ef3641a9c2adb0ed7351940725e419960adaa3dd
anacmolina <53834507+anacmolina@users.noreply.github.com>
2019-09-29 20:53:55 -0500

    Archivo BASH Parcial1
    Este codigo tiene como default python (también funciona python3)

--- Parciales/Parcial1/N1044508251/Parcial1.sh
+++ Parciales/Parcial1/N1044508251/Parcial1.sh
@@ -2,4 +2,4 @@
 g++ -o P1.out Punto1.cpp;  #Se compila el codigo de c++ de primer punto
 ./P1.out;                   #Se corre el ejecutable
 #Corre el segundo punto
-python3 Punto2.py;          #Se corre el codigo del segundo punto
+python Punto2.py;          #Se corre el codigo del segundo punto
